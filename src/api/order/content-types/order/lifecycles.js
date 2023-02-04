38ee7c5e78739211b5d8026f4b7652eb3d8ff69b
davidhanenko <d.hanenko24@gmail.com>
2023-02-03 19:32:09 -0500

    email sending after ordrer create

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -48,8 +48,10 @@ module.exports = {
         text: "Your order received. We will contact you about order details as soon as our representative reviewed it.",
         html: `<div>
             <h2 style="color: blue">
-              You received new order.
+              You received a new order.
             </h2>
+
+            <hr />
 
             <h4 style="color: blue">Order details:</h4>
             <p style="margin: 0">Charge: $${charge.toFixed(2)}</p>
@@ -60,7 +62,7 @@ module.exports = {
             <p style="color: green">Follow the <a href="https://a2z-ui-demo.vercel.app/orders">link</a> to review it.</p>
 
             <hr  style="color: blue" />
-            <h4>Client contacts:</h4>
+            <h4 style="margin: 10px 0">Client contacts:</h4>
             <p style="margin: 0">${name}</p>
             <p style="margin: 0">${company}</p>
             <p style="margin: 0">${phone}</p>
